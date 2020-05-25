b32b814c1a2b7912319f052dee2c20257ed7e519
ItalyPaleAle <43508+ItalyPaleAle@users.noreply.github.com>
2020-05-25 14:17:45 -0700

    Navbar shows the user's profille pic

--- web/src/components/Navbar.svelte
+++ web/src/components/Navbar.svelte
@@ -3,23 +3,30 @@
     <div class="my-4 pl-4 md:pl-0 text-lg">
       <i class="fa fa-pencil-square-o" aria-hidden="true"></i> Journal
     </div>
-    <div class="my-4 pr-4 md:pr-0">
-      <span
-        class="ml-4 cursor-pointer {(active == 'home') ? 'text-blue-600 underline' : ''}"
-        on:click={() => $view = null}>
-        Home
-      </span>
-      <span
-        class="ml-4 cursor-pointer {(active == 'add') ? 'text-blue-600 underline' : ''}"
-        on:click={() => $view = 'add'}>
-        Add
-      </span>
+    <div class="pr-4 md:pr-0">
+      <div class="my-4 inline-block">
+        <span class="ml-4 cursor-pointer {(active == 'home') ? 'text-blue-600 underline' : ''}"
+          on:click={() => $view = null}>
+          Home
+        </span>
+        <span class="ml-4 cursor-pointer {(active == 'add') ? 'text-blue-600 underline' : ''}"
+          on:click={() => $view = 'add'}>
+          Add
+        </span>
+      </div>
+      {#if $isAuthenticated}
+        <img src={$profile.picture}
+          class="ml-6 w-auto h-12 inline-block"
+          alt="{$profile.name} picture"
+          title={$profile.name}
+          />
+      {/if}
     </div>
   </div>
 </nav>
 
 <script>
-import {view} from '../stores.js'
+import {profile, isAuthenticated, view} from '../stores.js'
 
 let active
 $: {
