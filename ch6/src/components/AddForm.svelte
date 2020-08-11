336a0ce9956e56621d242edd5abc93d542282520
ItalyPaleAle <43508+ItalyPaleAle@users.noreply.github.com>
2020-08-10 21:12:49 -0700

    Missing id in input

--- ch6/src/components/AddForm.svelte
+++ ch6/src/components/AddForm.svelte
@@ -4,6 +4,7 @@
       class="block text-gray-700 font-bold mb-2">
       Title:</label>
     <input type="text"
+      id="addform-title"
       bind:value={title}
       class="shadow-inner appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" />
   </div>
